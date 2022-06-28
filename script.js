e82a3e3f916c69f7de4c7b271307322d4344499e
Rafael Lima <rafa.lima.01@live.com>
2022-06-27 22:06:02 -0300

    testando offset()

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
       mousePosition.y = event.pageY;
     });
 
-    $('.tooltips').click(function (event) {
+    /*$('.tooltips').click(function (event) {
       let elementWidth = $(event.target).innerWidth();
       let windowWidth = $(window).width();
       let elementheight = $(event.target).innerHeight();
@@ -20,16 +20,35 @@
         top: topPosition + elementheight,
       });
       console.log(windowWidth);
+    });*/
+
+    $('.tooltips').click(function (event) {
+      let elementWidth = $(event.target).innerWidth();
+      let windowWidth = $(window).width();
+      let elementheight = $(event.target).innerHeight();
+      let leftPosition = $(this).offset().left;
+      let topPosition = event.pageY;
+      $('.tooltips-content')
+        .offset({
+          left: ((leftPosition - windowWidth) * 50) / 100,
+        })
+        .css({
+          display: 'inline-block',
+          position: 'absolute',
+          top: topPosition + elementheight,
+        });
+      console.log('leftPosition', leftPosition);
+      console.log('topPosition', topPosition);
     });
 
-    $('.tooltips').mouseleave(function () {
+    /*$('.tooltips').mouseleave(function () {
       $('.tooltips-content').css({
         display: 'none',
         position: 'initial',
         left: 'initial',
         top: 'initial',
       });
-    });
+    });*/
   }
 
   const init = (function () {
